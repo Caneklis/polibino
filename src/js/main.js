57a2e4fbfd692b5f408199da80ec138272e6d648
alesukhin <alesukhin@museumplus.ru>
2021-07-20 00:19:15 +0300

    add fix header

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,23 @@ document.addEventListener("DOMContentLoaded", () => {
   // eslint-disable-next-line no-console
   console.log("DOM полностью загружен и разобран");
   require("./modules/main-nav");
+
+  const header = document.querySelector(".header");
+  console.log(header);
+  let scrollPrev = 0;
+
+  window.addEventListener("scroll", () => {
+    let scrolled = window.pageYOffset;
+
+    if (scrolled > 10 && scrolled > scrollPrev) {
+      header.classList.add("header--out");
+      header.classList.remove("header--in");
+    } else if (scrolled < 10) {
+      header.classList.remove("header--in");
+    } else {
+      header.classList.remove("header--out");
+      header.classList.add("header--in");
+    }
+    scrollPrev = scrolled;
+  });
 });
