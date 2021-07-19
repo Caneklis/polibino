017d85fd395f6d8d29ad8284af47eb94942fdce5
alesukhin <alesukhin@museumplus.ru>
2021-07-19 23:54:53 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // eslint-disable-next-line no-console
+  console.log("DOM полностью загружен и разобран");
+  require("./modules/main-nav");
+});
